fa61ddad8c0d51990c16b5343661053b1a4db0c3
Houssame <hossame@inbox.ru>
2024-10-21 21:04:09 +0100

    very good result

--- taxi6.js
+++ taxi6.js
@@ -5,12 +5,22 @@ import Tesseract from 'tesseract.js';
 
 config();
 
-async function nextHour() {
+async function timeUntilNextHour() {
+    const now = new Date();
+    
+    const currentMinutes = now.getMinutes();
+    const currentSeconds = now.getSeconds();
+    const currentMilliseconds = now.getMilliseconds();
+    
+    const millisecondsUntilNextHour = 
+        ((60 - currentMinutes) * 60 * 1000) -
+        (currentSeconds * 1000) -            
+        currentMilliseconds;                 
 
-    let time = new Date();
-    console.log('current hour ===>', time.getHours());
-    // while (time.getHours() === new Date().getHours()) {}
+    console.log('time in MS until next hour', millisecondsUntilNextHour);
+    await sleep(millisecondsUntilNextHour);
 }
+
 
 async function captchaResolver(page) {
     let svgString = await page.evaluate(() => {
@@ -35,23 +45,20 @@ async function captchaResolver(page) {
         let btn = await page.$('input[placeholder="Enter captcha"]');
         if (btn) {
             text = text.replace(' ', '');
-            console.log('text: |' + text + '|', typeof(text));
+            console.log('text: |' + text + '|');
             await page.type('input[placeholder="Enter captcha"]', text);
             let btn2 = await page.$('.flex.gap-4.justify-center.mt-4');
             if (btn2) {
-                console.log('Here');
                 await btn2.click()
             }
-            await sleep(200);
+            await sleep(300);
             let wrongCaptcha = await page.$('div.ReactModal__Content');
             if (wrongCaptcha) {
-                // console.log(wrongCaptcha);
                 console.log('wrong captcha!');
                 await page.click('input[placeholder="Enter captcha"]', {clickCount: 3});
                 await page.keyboard.press('Backspace')
                 await captchaResolver(page);
             } else {
-                // console.log(wrongCaptcha);
                 console.log('reserved');
                 return;
             }
@@ -59,6 +66,19 @@ async function captchaResolver(page) {
     } else {
         console.error('SVG not found on the page.');
     }
+}
+
+async function signin(browser) {
+    const page = await browser.newPage();
+    await page.goto('https://bus-med.1337.ma/api/auth/42');
+    await page.type('#username', process.env.USER);
+    await page.type('#password', process.env.PASSWORD);
+    await page.click('#kc-login');
+    await page.waitForSelector('.flex.flex-col.space-y-3 .text-center p.text-lg.font-semibold');
+    await timeUntilNextHour();
+    await page.goto('https://bus-med.1337.ma/api/auth/42');
+    await sleep(300);
+    return page;
 }
 
 async function sleep(ms) {
@@ -70,21 +90,14 @@ async function sleep(ms) {
         executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         headless: process.env.BROWSER_MODE === 'headless'
     });
-    const page = await browser.newPage();
 
-    await page.goto('https://bus-med.1337.ma/api/auth/42');
-    await page.type('#username', process.env.USER);
-    await page.type('#password', process.env.PASSWORD);
-    await page.click('#kc-login');
-    await nextHour();
-    await page.waitForSelector('.flex.flex-col.space-y-3 .text-center p.text-lg.font-semibold');
+    const page = await signin(browser);
     const buses = await page.$$('.bg-white.rounded-xl.shadow-lg');
-
     for (let bus of buses) {
         const destination = await bus.$eval('.flex.items-center.justify-between .text-center:last-child', el => el.innerText);
-
+    
         console.log('|' + destination.replace(/^TO\n\n/, '') + '|');
-
+    
         if (destination.replace(/^TO\n\n/, '') === 'MARTIL') {
             console.log("Martil bus found. Booking seat...");
             const bookButton = await bus.$('button.bg-green-500');
@@ -92,11 +105,11 @@ async function sleep(ms) {
             const buttonSelector = 'button[data-slot="trigger"]';
             await page.waitForSelector(buttonSelector);
             await page.click(buttonSelector);
-
             await page.click('.w-full.relative.flex.flex-col.gap-1.p-1:first-child');
             let time = new Date();
             await captchaResolver(page);
-            console.log(time.getMilliseconds() - new Date().getMilliseconds());
+            // console.log(time.getMilliseconds() - new Date().getMilliseconds());
+            break;
         }
     }
     await browser.close();
