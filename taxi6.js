018aea34f79d090e4742ab8ca1b51def8b57a2b5
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2024-10-22 22:27:47 +0100

    Update taxi6.js

--- taxi6.js
+++ taxi6.js
@@ -17,8 +17,8 @@ async function timeUntilNextHour() {
         (currentSeconds * 1000) -            
         currentMilliseconds;                 
 
-    console.log('time in MS until next hour', (millisecondsUntilNextHour - 15000));
-    await sleep((millisecondsUntilNextHour - 15000));
+    console.log('time in MS until next hour', (millisecondsUntilNextHour - 5000));
+    await sleep((millisecondsUntilNextHour - 5000));
 }
 
 
